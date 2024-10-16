c92867f11a2b37f044e34f2b4a85423f9c1f513a
salesenric <32782205+salesenric@users.noreply.github.com>
2024-10-16 13:50:18 +0200

    Create api.js

---
+++ Quality Assurance/Metric-Imperial Converter/routes/api.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+
+  let convertHandler = new ConvertHandler();
+
+  app.get('/api/convert', (req, res) => {
+    const input = req.query.input;
+
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+
+    // Updated error handling
+    if (initNum === "invalid number" && initUnit === "invalid unit") {
+      return res.send("invalid number and unit");
+    }
+    if (initNum === "invalid number") {
+      return res.send("invalid number");
+    }
+    if (initUnit === "invalid unit") {
+      return res.send("invalid unit");
+    }
+
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string
+    });
+  });
+};
